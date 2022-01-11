e8e038c8a148a968a6076d3fef3534df26177480
fauzyahadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-11 14:35:15 +0700

    Praktikum 2.1 Algoritma Sin Cos

--- Grafika.java
+++ Grafika.java
@@ -1,9 +1,8 @@
 /*
  * Nama    : Fauzyah Hadirahma
  * NPM     : 20191310070
- * Tugas   : Komputer Grafis Multimedia - Algoritma Bresenham
  */
-package AlgoritmaBresenham;
+package AlgoritmaSinCos;
 
 /**
  *
@@ -17,40 +16,31 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Grafika extends JPanel {
-   
+    int xc = 0;
+    int yc = 0;
+    
+    public Grafika(){
+        xc = 600 / 2;
+        yc = 600 / 2;
+    }
+    
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+        
         Graphics2D gd = (Graphics2D) g;
         
-        int r =150;
-        int d = (5 / 4) * r;
-        int x = 0;
-        int y = r;
+        int r = 200, x = 0, y = 0;
         
-        do{
-            gd.setColor(Color.RED);
-            gd.drawLine(y + 200, x + 200, y + 200, x + 200);
-            gd.drawLine(x + 200, y + 200, x + 200, y + 200);
-            gd.drawLine(x + 200, -y + 200, x + 200, -y + 200);
-            gd.drawLine(y + 200, -x + 200, y + 200, -x + 200);
-            gd.drawLine(-y + 200, -x + 200, -y + 200, -x + 200);
-            gd.drawLine(-x + 200, -y + 200, -x + 200, -y + 200);
-            gd.drawLine(-x + 200, y + 200, -x + 200, y + 200);
-            gd.drawLine(-y + 200, x + 200, -y + 200, x + 200);
-            
-            if (d<0){
-                d = d + 2 * x + 3;
-            } else {
-                d = d + 2 * (x - y) + 5;
-                y = y - 1;
-            }
-            x = x + 1;
-        } while (x < y);
+        for(int i=0; i<=360; i++){
+            x=(int) ((Math.cos((int) i) *r) + xc);
+            y=(int) ((Math.sin((int) i) *r) + yc);
+            gd.drawLine(x, y, x, y);
+        }
     }
     
     public static void main(String[] args){
-        Grafika g  = new Grafika();
+        Grafika g = new Grafika();
         JFrame jf = new JFrame();
         jf.add(g);
         jf.setSize(600, 600);
@@ -59,4 +49,3 @@ public class Grafika extends JPanel {
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-
